129d540582cca3fb573bf7bfd3ba17f9e6d4f272
Simon Perepelitsa <sema@sema.in>
2020-01-23 16:40:45 +0300

    Set gem home according to minor version like default
    For example, Ruby 2.6.5 should have GEM_HOME as ~/.gem/ruby/2.6.0, not ~/.gem/ruby/2.6.5
    
    This follows the same logic as the default gem home.

--- share/chruby/functions/chruby.fish
+++ share/chruby/functions/chruby.fish
@@ -49,24 +49,23 @@ function chruby_reset
 
   set -e RUBY_ROOT
   set -e RUBY_ENGINE
-  set -e RUBY_VERSION
   set -e GEM_HOME
 end
 
 # Add specified Ruby and its gems to PATH.
 # Set GEM_HOME (gem install path) to e. g. ~/.gem/ruby/2.1.0
-# Also set RUBY_ROOT, RUBY_ENGINE, RUBY_VERSION for your own use.
+# Also set RUBY_ROOT, RUBY_ENGINE for your own use.
 function chruby_use
   set -g RUBY_ROOT $argv
   set -xg PATH $RUBY_ROOT/bin $PATH
 
   # Get info from ruby.
-  set -l ruby_code "puts RUBY_ENGINE, RUBY_VERSION, Gem.default_dir"
+  set -l ruby_code "puts RUBY_ENGINE, RbConfig::CONFIG['ruby_version'], Gem.default_dir"
   set -l ruby_vars (ruby -e $ruby_code)
 
   set -g RUBY_ENGINE $ruby_vars[1]
-  set -g RUBY_VERSION $ruby_vars[2]
-  set -g GEM_ROOT $ruby_vars[3]
+  set -l RUBY_VERSION $ruby_vars[2]
+  set -l GEM_ROOT $ruby_vars[3]
 
   set -xg GEM_HOME $HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION
 
